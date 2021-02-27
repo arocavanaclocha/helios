dac6838d35339584899c44fecf8922e8333a9fdd
Alberto <75672168+arocavanaclocha@users.noreply.github.com>
2021-02-27 13:27:18 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,35 +1,53 @@
+# Helios > Create instance
+helios = Helios()
 
-# Architect > Start
-architect = Architect()
+#-------------------------------------------------------------------
+# Helios > Development > Describe the testing scenario
+#-------------------------------------------------------------------
+# Define the current testing Scenario:
+#  * name and remarks  
+#  * created_date, updated_date
+#  * the spatial scenario cube, where the DUT (Device Under Test) is in the center (default shape is 3x3x3)
+helios.Scenario.new(name="Scenario: Laboratory iMM (PR3808)", remarks="Tests for defining most appropiate light for demo.", spatial_scenario_cube_shape=3)
 
-# Architect > Describe scenario
-architect.Scenario.new(name="Scenario: Laboratory iMM (PR3808)", remarks="Tests for defining most appropiate light for demo.", spatial_scenario_cube_shape=3)
+#-------------------------------------------------------------------
+# Helios > Development > Manage Luminaires 
+#-------------------------------------------------------------------
+# Defines the LuminaireSet, that is:
+#  * all Luminaires available in the scenario
+#  * if the light is White Dimable, CCT Tunable or RGB
+#  * a unique name for referencing by name
+#  * and the spatial scenario cube position in the scenario 
+helios.Scenario.LuminaireSet.add(luminaire_args=[], luminaire_name="Top light", luminaire_start_channel=1, luminaire_type=[1,1,0], luminaire_xyz=helios.Luminaire.FrontTop())
+helios.Scenario.LuminaireSet.add(luminaire_args=[], luminaire_name="Left light", luminaire_start_channel=2, luminaire_type=[1,1,0], luminaire_xyz=helios.Luminaire.FrontLeft())
+helios.Scenario.LuminaireSet.add(luminaire_args=[], luminaire_name="Right light", luminaire_start_channel=3, luminaire_type=[1,1,0], luminaire_xyz=helios.Luminaire.FrontRight())
+helios.Scenario.LuminaireSet.add(luminaire_args=[], luminaire_name="Bottom light", luminaire_start_channel=4, luminaire_type=[1,1,0], luminaire_xyz=helios.Luminaire.FrontBottom())
+helios.Scenario.LuminaireSet.add(luminaire_args=[], luminaire_name="RGB perimetral ambient", luminaire_start_channel=5, luminaire_type=[1,1,1], luminaire_xyz=helios.Luminaire.FrontPerimetral())
 
-# Architect > Compose scenario lighting
-architect.Scenario.LuminaireSet.add(luminaire_args=[], luminaire_name="Top light", luminaire_start_channel=1, luminaire_type=[1,1,0], luminaire_xyz=helios.Luminaire.FrontTop())
-architect.Scenario.LuminaireSet.add(luminaire_args=[], luminaire_name="Left light", luminaire_start_channel=2, luminaire_type=[1,1,0], luminaire_xyz=helios.Luminaire.FrontLeft())
-architect.Scenario.LuminaireSet.add(luminaire_args=[], luminaire_name="Right light", luminaire_start_channel=3, luminaire_type=[1,1,0], luminaire_xyz=helios.Luminaire.FrontRight())
-architect.Scenario.LuminaireSet.add(luminaire_args=[], luminaire_name="Bottom light", luminaire_start_channel=4, luminaire_type=[1,1,0], luminaire_xyz=helios.Luminaire.FrontBottom())
-architect.Scenario.LuminaireSet.add(luminaire_args=[], luminaire_name="RGB perimetral ambient", luminaire_start_channel=5, luminaire_type=[1,1,1], luminaire_xyz=helios.Luminaire.FrontPerimetral())
+#-------------------------------------------------------------------
+# Helios > Development > Plan scenes' screenplay
+#-------------------------------------------------------------------
+helios.Scenario.ScreenPlay.add(scene_name="Scene #1")
+helios.Scenario.ScreenPlay.add(scene_name="Scene #2")
 
-# Architect > Plan scenes screenplay
-architect.Scenario.ScreenPlay.add(scene_name="Scene #1")
-architect.Scenario.ScreenPlay.add(scene_name="Scene #2")
-
-# Architect > Configure light scene for each scene
-architect.Scenario.compose_lights()
+#-------------------------------------------------------------------
+# Helios > Development > Compose scenes' lighting
+#-------------------------------------------------------------------
+helios.Scenario.compose_lights()
 
 scene = 0
-#architect.Scenario.Light
+#helios.Scenario.Light
 #   ...brightness(scene=scene, step=+50, luminaire=-1)
 #   ...set_brightness(scene=scene, value=255, luminaire=-1)
 #   ...set_cct(scene=scene, value=155, luminaire=-1)
 #   ...cct(scene=scene, step=-25, luminaire=-1)
 #   ...switch(luminaire=-1)
 #
-architect.Scenario.Light.set_cct(scene=scene, value=155, luminaire=-1)
+helios.Scenario.Light.set_cct(scene=scene, value=155, luminaire=-1)
 
-#Architect > Frameset dynamics editor
+#-------------------------------------------------------------------
+# Helios > Development > Frameset dynamics editor
+#-------------------------------------------------------------------
 #
 # Scene:      |00--------------------------->01|01--------------------------->02|
 # Frame:      |00|01|02|03|04|05|06|07|08|09|10|00|01|02|03|04|05|06|07|08|09|10|
@@ -44,8 +62,8 @@ architect.Scenario.Light.set_cct(scene=scene, value=155, luminaire=-1)
 
 #Architect > Frameset dynamics editor > Define sequence and frames
 # Order sequence of scenes and set the number of frames for each scene transition
-architect.Scenario.Frameset.sequence=[0,1,2]
-architect.Scenario.Frameset.nr_frames=[25,25]
+helios.Scenario.Frameset.sequence=[0,1,2]
+helios.Scenario.Frameset.nr_frames=[25,25]
 
 
 #Architect > Frameset dynamics editor > Define times [msec]
